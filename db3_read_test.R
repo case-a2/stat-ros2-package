fec55519b31d704b305c7c8a65ec783569a9a761
Case <caseashton2@gmail.com>
2023-12-10 21:27:27 -0600

    Parsed the database format of bag file for the message data column

--- db3_read_test.R
+++ db3_read_test.R
@@ -4,15 +4,33 @@
 
 
 # ROS 2 Bag files are .db3 files (SQLite database files)
-install.packages("RSQLite")
-install.packages("dbDriver")
+# Install package for reading SQL db files if you haven't
+
+# install.packages(c("RSQLite", "DBIDriver", "ggplot2"))
+install.packages('dplyr')
+library(RSQLite)
+library(DBI)
+library(ggplot2)
+library(dplyr)
+
 
 # This file will test the unpacking of the turtlebot tutorials the (subset/) folder
 subset <- "subset/subset_0.db3"
-sqlite.driver <- dbDriver("SQLite")
-db <- dbConnect(sqlite.driver,
-                dbname = subset)
+ur3_move <- "ur3_move/ur3_move_0.db3"
+ur <- dbConnect(RSQLite::SQLite(), dbname = ur3_move)
+tables <- dbListTables(ur)
+print(tables)
 
-## some operations
-dnListTables(db)
-mytable <- dbReadTable(db, "testing table")
+message_data <- dbReadTable(ur, "messages")
+
+sqlite.driver <- dbDriver("SQLite")
+
+turt <- dbConnect(RSQLite::SQLite(), dbname = subset)
+turt_message <- dbReadTable(turt, "messages")
+
+query <- "SELECT data FROM messages"
+
+blob_data <- dbGetQuery(ur, query)
+
+
+
